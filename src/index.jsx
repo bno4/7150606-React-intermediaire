50689e71b3c8ec8a0ab771cab5f83936f7df6f81
bno4 <bnduchemin@gmail.com>
2023-05-16 15:23:18 +0200

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Survey from './pages/Survey'
-import Results from './pages/Results'
-import Freelances from './pages/Freelances'
-import Header from './components/Header'
-import Error from './components/Error'
-import { createGlobalStyle } from 'styled-components'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Survey from './pages/Survey';
+import Results from './pages/Results';
+import Freelances from './pages/Freelances';
+import Header from './components/Header';
+import Error from './components/Error';
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     * {
       font-family: 'Trebuchet MS', Helvetica, sans-serif;
     }
-`
+`;
 
 ReactDOM.render(
   <React.StrictMode>
@@ -30,4 +30,4 @@ ReactDOM.render(
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
