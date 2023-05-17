97d714fda03e7c0f101ac1ab32c20d3a0a752db4
bno4 <bnduchemin@gmail.com>
2023-05-17 14:54:38 +0200

    updt

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -62,6 +62,13 @@ function formatFetchParams(answers) {
   }, '')
 }
 
+export function formatJobList(title, listLength, index) {
+  if (index === listLength - 1) {
+    return title
+  }
+  return `${title},`
+}
+
 function Results() {
   const { theme } = useTheme()
   const { answers } = useContext(SurveyContext)
@@ -91,8 +98,7 @@ function Results() {
               key={`result-title-${index}-${result.title}`}
               theme={theme}
             >
-              {result.title}
-              {index === resultsData.length - 1 ? '' : ','}
+              {formatJobList(result.title, resultsData.length, index)}
             </JobTitle>
           ))}
       </ResultsTitle>
