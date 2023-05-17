093aa6ed638ce58f4256dc20ee2d6779714db34a
bno4 <bnduchemin@gmail.com>
2023-05-17 14:58:54 +0200

    updt

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,4 +1,5 @@
 import { formatJobList } from './'
+import { formatQueryParams } from './'
 
 describe('The formatJobList function', () => {
   it('should add a comma to a word', () => {
@@ -10,3 +11,14 @@ describe('The formatJobList function', () => {
     expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
   })
 })
+
+describe('The formatQueryPamrams function', () => {
+    it('should add a comma to a word', () => {
+      const expectedState = 'item2,'
+      expect(formatQueryParams('item2', 3, 1)).toEqual(expectedState)
+    })
+    it('should not add a comma to the last element of the list', () => {
+      const expectedState = 'item3'
+      expect(formatQueryParams('item3', 3, 2)).toEqual(expectedState)
+    })
+  })
